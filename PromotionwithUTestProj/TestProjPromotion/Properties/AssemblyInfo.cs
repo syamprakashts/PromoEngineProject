26a8c2255f5b43b62720f56d623c9d5acc422819
syamprakashts <71143060+syamprakashts@users.noreply.github.com>
2020-09-15 13:38:42 +0530

    Add files via upload

---
+++ PromotionwithUTestProj/TestProjPromotion/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProjPromotion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProjPromotion")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fbb66e8-e7d5-49dd-9530-0209b705320e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PromotionwithUTestProj/TestProjPromotion/TestPromoEngine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionProj;
+using System.Collections.Generic;
+namespace TestProjPromotion
+{
+    [TestClass]
+    public class TestPromoEngine
+    {
+        public List<SKUs> DefinedSKUs = new List<SKUs>(); //List of all SKUs
+        public List<ActivePromo> curActivePromo = new List<ActivePromo>(); //Active Promotions
+        public List<inputSKUs> curInputSKUsA = new List<inputSKUs>(); //scenarioA input SKUs
+        public List<inputSKUs> curInputSKUsB = new List<inputSKUs>(); //scenarioA input SKUs
+        public List<inputSKUs> curInputSKUsC = new List<inputSKUs>(); //scenarioA input SKUs
+
+        public TestPromoEngine()
+        {
+            //initialise SKU values
+            DefinedSKUs.Add(new SKUs('A', 50));
+            DefinedSKUs.Add(new SKUs('B', 30));
+            DefinedSKUs.Add(new SKUs('C', 20));
+            DefinedSKUs.Add(new SKUs('D', 15));
+
+            //initialise active promotions
+            curActivePromo.Add(new ActivePromo(1, DefinedSKUs[0], null, 3, 130));
+            curActivePromo.Add(new ActivePromo(1, DefinedSKUs[1], null, 2, 45));
+            curActivePromo.Add(new ActivePromo(2, DefinedSKUs[2], DefinedSKUs[3], 1, 30));
+
+            //Scenario A
+            curInputSKUsA.Add(new inputSKUs(DefinedSKUs[0], 1));
+            curInputSKUsA.Add(new inputSKUs(DefinedSKUs[1], 1));
+            curInputSKUsA.Add(new inputSKUs(DefinedSKUs[2], 1));
+
+            //Scenario B
+            curInputSKUsB.Add(new inputSKUs(DefinedSKUs[0], 5));
+            curInputSKUsB.Add(new inputSKUs(DefinedSKUs[1], 5));
+            curInputSKUsB.Add(new inputSKUs(DefinedSKUs[2], 1));
+
+            //Scenario C
+            curInputSKUsC.Add(new inputSKUs(DefinedSKUs[0], 3));
+            curInputSKUsC.Add(new inputSKUs(DefinedSKUs[1], 5));
+            curInputSKUsC.Add(new inputSKUs(DefinedSKUs[2], 1));
+            curInputSKUsC.Add(new inputSKUs(DefinedSKUs[3], 1));
+        }
+        [TestMethod]
+        public void TestPromoEngineForScenarioA()
+        {
+            Assert.AreEqual(SKUUtilities.EstimateTotalWithPromotionEngine(curActivePromo, curInputSKUsA),100);
+        }
+        [TestMethod]
+        public void TestPromoEngineForScenarioB()
+        {
+            Assert.AreEqual(SKUUtilities.EstimateTotalWithPromotionEngine(curActivePromo, curInputSKUsB), 370);
+        }
+        [TestMethod]
+        public void TestPromoEngineForScenarioC()
+        {
+            Assert.AreEqual(SKUUtilities.EstimateTotalWithPromotionEngine(curActivePromo, curInputSKUsC), 280);
+        }
+    }
+}
